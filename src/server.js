76ad4910d9111747cd7bc2b1f2251561e192acde
samuelxses@unipam.edu.br <Samuel Ximenes>
2020-11-11 12:13:19 -0300

    Heroku fix

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 require('dotenv').config()
 require('./database')
 
-app.listen(process.env.APP_PORT || 3000)
+app.listen(process.env.PORT || 3000)
 app.use(express.json())
 app.use(routes)
 app.use(cors())
